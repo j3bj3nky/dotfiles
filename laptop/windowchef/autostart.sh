f78bc3b89da7c47f805dc5c87371e22906d88114
derReineke <35681013+derReineke@users.noreply.github.com>
2018-02-01 17:42:27 -0500

    Fixed everything...

--- laptop/windowchef/autostart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# autostart for windowchef
-
-windowchef
-sxkhd &
-lemonbar -p &
-i3status --config ~/.config/status-windowchef.conf
-compton --config ${HOME}/.config/compton/compton.conf -b
-exec feh --bg-scale ${HOME}/Pics/cybergamebak.jpg &
